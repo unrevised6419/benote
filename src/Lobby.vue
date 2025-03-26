3974bcc3ffb95367e4a489add092a75ef3001322
Andrei Luca <1881266+iamandrewluca@users.noreply.github.com>
2025-03-26 03:37:08 +0200

    feat: add router and empty lobby

---
+++ src/Lobby.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+<template>
+	<div>
+		<RouterLink to="/play" class="underline">Play ▶️</RouterLink>
+		<div>TODO: Lobby</div>
+	</div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from "vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
+import { createWebHistory, createRouter } from "vue-router";
 import "./style.css";
 import App from "./App.vue";
+import Board from "./Board.vue";
+import Lobby from "./Lobby.vue";
 
-createApp(App).use(VueQueryPlugin).mount("#app");
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: "/", component: Lobby },
+		{ path: "/:id", component: Board },
+	],
+});
+
+createApp(App).use(router).use(VueQueryPlugin).mount("#app");
