3f1111080c4063083b7b37be0392f6d5eeceae12
Andrei Luca <1881266+iamandrewluca@users.noreply.github.com>
2025-04-12 02:41:04 +0300

    feat: add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import { Analytics } from "@vercel/analytics/vue";
 import PWABadge from "./components/PWABadge.vue";
 </script>
 
 <template>
+	<Analytics />
 	<div
 		class="border-base-300 container mx-auto min-h-screen max-w-md border-x px-4 py-4"
 	>
